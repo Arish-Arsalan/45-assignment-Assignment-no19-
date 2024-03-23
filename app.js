b2429e556bcf6d8cef0b59d087f50a4e8c855445
Arish Arsalan <159803393+Arish-Arsalan@users.noreply.github.com>
2024-03-23 22:20:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+let languages = ['english', 'korean', 'urdu', 'thai', 'arabic', 'french'];
+console.log("list of languages:");
+for (let top of languages) {
+    console.log(top);
+}
---
+++ app.ts
@@ -0,0 +1,5 @@
+let languages : string[] = ['english','korean','urdu','thai','arabic','french']
+console.log("list of languages:")
+for(let top of languages){
+    console.log(top)
+}
